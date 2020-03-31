e23db3e8ebca2ae9681f981474965aed7846c5d6
NightRoadIx <quintanarxbio@gmail.com>
2020-03-31 01:31:19 -0600

    Add files via upload

---
+++ imaginasizacion/imaginasizacion/MainForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: NightRoadIx
+ * Date: 17/03/2020
+ * Time: 1:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace imaginasizacion
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(411, 317);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 336);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(410, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Buscar Imagen";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(13, 377);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(410, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "label1";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(457, 12);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Nombre archivo:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(564, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(692, 10);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(104, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Salvar";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(815, 410);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "imaginasizacion";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
---
+++ imaginasizacion/imaginasizacion/MainForm.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: NightRoadIx
+ * Date: 17/03/2020
+ * Time: 1:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace imaginasizacion
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string nombreImagen;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			label1.TextAlign = ContentAlignment.MiddleCenter;
+			label1.Text = "Image";
+			// Desactivar botón salvar
+			button2.Enabled = false;
+			
+			nombreImagen = "";
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			// Instanciando el objeto OpenFileDialog
+			OpenFileDialog opf = new OpenFileDialog();
+			
+			opf.Filter = "Elegir una imagen (*.jpg;*.png;*.gif)|*.jpg;*.png;*.gif";
+			
+			if( opf.ShowDialog() == DialogResult.OK )
+			{
+				pictureBox1.Image = Image.FromFile(opf.FileName);
+				nombreImagen = opf.FileName;
+				label1.Text = opf.FileName;
+			}
+		}
+		
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			if( (textBox1.Text.Trim().Length > 0)&&( nombreImagen != "" ) )
+				button2.Enabled = true;
+			else
+				button2.Enabled = false;
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			File.WriteAllText((textBox1.Text+".txt"), nombreImagen);
+		}
+	}
+}
