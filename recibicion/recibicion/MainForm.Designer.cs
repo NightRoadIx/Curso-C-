14880f5109f51a9c9812ad8d5c8f7c433c38c70b
NightRoadIx <quintanarxbio@gmail.com>
2020-04-01 01:33:05 -0600

    Add files via upload

---
+++ recibicion/recibicion/MainForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: NightRoadIx
+ * Date: 31/03/2020
+ * Time: 23:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace recibicion
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.comboBox4 = new System.Windows.Forms.ComboBox();
+			this.comboBox5 = new System.Windows.Forms.ComboBox();
+			this.comboBox6 = new System.Windows.Forms.ComboBox();
+			this.comboBox7 = new System.Windows.Forms.ComboBox();
+			this.comboBox8 = new System.Windows.Forms.ComboBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(305, 291);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(128, 29);
+			this.button1.TabIndex = 16;
+			this.button1.Text = "Ver";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Dominio:";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(12, 53);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Reino:";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(12, 134);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Orden:";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(12, 91);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Clase:";
+			// 
+			// label8
+			// 
+			this.label8.Location = new System.Drawing.Point(12, 296);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(100, 23);
+			this.label8.TabIndex = 8;
+			this.label8.Text = "Raza:";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(12, 253);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(100, 23);
+			this.label7.TabIndex = 10;
+			this.label7.Text = "Especie:";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(12, 213);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 23);
+			this.label6.TabIndex = 12;
+			this.label6.Text = "Género:";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(12, 175);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 23);
+			this.label5.TabIndex = 14;
+			this.label5.Text = "Familia:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+									"Archae",
+									"Bacteria",
+									"Eukarya"});
+			this.comboBox1.Location = new System.Drawing.Point(77, 9);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(173, 21);
+			this.comboBox1.TabIndex = 17;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(77, 50);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(173, 21);
+			this.comboBox2.TabIndex = 18;
+			this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2SelectedIndexChanged);
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Location = new System.Drawing.Point(77, 88);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(173, 21);
+			this.comboBox3.TabIndex = 19;
+			this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.ComboBox3SelectedIndexChanged);
+			// 
+			// comboBox4
+			// 
+			this.comboBox4.FormattingEnabled = true;
+			this.comboBox4.Location = new System.Drawing.Point(77, 131);
+			this.comboBox4.Name = "comboBox4";
+			this.comboBox4.Size = new System.Drawing.Size(173, 21);
+			this.comboBox4.TabIndex = 20;
+			this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.ComboBox4SelectedIndexChanged);
+			// 
+			// comboBox5
+			// 
+			this.comboBox5.FormattingEnabled = true;
+			this.comboBox5.Location = new System.Drawing.Point(77, 172);
+			this.comboBox5.Name = "comboBox5";
+			this.comboBox5.Size = new System.Drawing.Size(173, 21);
+			this.comboBox5.TabIndex = 21;
+			this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.ComboBox5SelectedIndexChanged);
+			// 
+			// comboBox6
+			// 
+			this.comboBox6.FormattingEnabled = true;
+			this.comboBox6.Location = new System.Drawing.Point(77, 210);
+			this.comboBox6.Name = "comboBox6";
+			this.comboBox6.Size = new System.Drawing.Size(173, 21);
+			this.comboBox6.TabIndex = 22;
+			this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.ComboBox6SelectedIndexChanged);
+			// 
+			// comboBox7
+			// 
+			this.comboBox7.FormattingEnabled = true;
+			this.comboBox7.Location = new System.Drawing.Point(77, 250);
+			this.comboBox7.Name = "comboBox7";
+			this.comboBox7.Size = new System.Drawing.Size(173, 21);
+			this.comboBox7.TabIndex = 23;
+			this.comboBox7.SelectedIndexChanged += new System.EventHandler(this.ComboBox7SelectedIndexChanged);
+			// 
+			// comboBox8
+			// 
+			this.comboBox8.FormattingEnabled = true;
+			this.comboBox8.Location = new System.Drawing.Point(77, 293);
+			this.comboBox8.Name = "comboBox8";
+			this.comboBox8.Size = new System.Drawing.Size(173, 21);
+			this.comboBox8.TabIndex = 24;
+			this.comboBox8.SelectedIndexChanged += new System.EventHandler(this.ComboBox8SelectedIndexChanged);
+			// 
+			// label9
+			// 
+			this.label9.Location = new System.Drawing.Point(305, 103);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(128, 107);
+			this.label9.TabIndex = 25;
+			this.label9.Text = "label9";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(463, 371);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.comboBox8);
+			this.Controls.Add(this.comboBox7);
+			this.Controls.Add(this.comboBox6);
+			this.Controls.Add(this.comboBox5);
+			this.Controls.Add(this.comboBox4);
+			this.Controls.Add(this.comboBox3);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "recibicion";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.ComboBox comboBox8;
+		private System.Windows.Forms.ComboBox comboBox7;
+		private System.Windows.Forms.ComboBox comboBox6;
+		private System.Windows.Forms.ComboBox comboBox5;
+		private System.Windows.Forms.ComboBox comboBox4;
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ recibicion/recibicion/MainForm.cs
@@ -0,0 +1,190 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: NightRoadIx
+ * Date: 31/03/2020
+ * Time: 23:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace recibicion
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		/* Método cuando la ventana se carga */
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			// Inahbilitar todos los controles excepto el Combobox1
+			comboBox2.Enabled = false;
+			comboBox3.Enabled = false;
+			comboBox4.Enabled = false;
+			comboBox5.Enabled = false;
+			comboBox6.Enabled = false;
+			comboBox7.Enabled = false;
+			comboBox8.Enabled = false;
+			button1.Enabled = false;
+			label9.Text = "";
+		}
+		
+		/* Método cuando se selecciona un valor en el ComboBox para el Dominio */
+		void ComboBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+			// Cuando se selecciona el Dominio Eukarya se rellenan los valores del segundo ComboBox
+			if( comboBox1.Text == "Eukarya" )
+			{
+				comboBox2.Items.Add("Protista");
+				comboBox2.Items.Add("Fungi");
+				comboBox2.Items.Add("Plantae");
+				comboBox2.Items.Add("Animalia");
+				// Se activa el combobox2
+				comboBox2.Enabled = true;
+			}
+		}
+		
+		/* Selección del Reino */
+		void ComboBox2SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string[] Clase = {"Aneelida", "Arthropoda", "Brachiopoda", "Bryozoa", "Chordata", "Echinodermata", "Hemicohordata", "Loricefera", "Mammalia", "Mollusca", "Nematoda", "Phoronida", "Rhombozoa", "Tardigrada"};
+			if(comboBox2.Text == "Animalia")
+			{
+				foreach(string c in Clase)
+					comboBox3.Items.Add(c);
+				comboBox3.Enabled = true;
+			}
+			else
+			{
+				comboBox3.Items.Clear();
+				comboBox3.Text = "";
+				comboBox3.Enabled = false;
+			}
+		}
+		
+		/* Selección de la Clase */
+		void ComboBox3SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string[] Orden = {"Artilodacta","Carnivora","Chiroptera","Dinocerata","Litopterna","Mulituberculata","Primates","Rodentia","Scadentia"};
+			if(comboBox3.Text == "Mammalia")
+			{
+				foreach(string c in Orden)
+					comboBox4.Items.Add(c);
+				comboBox4.Enabled = true;
+			}
+			else
+			{
+				comboBox4.Items.Clear();
+				comboBox4.Text = "";
+				comboBox4.Enabled = false;
+			}
+		}
+		
+		/* Selección del Orden */
+		void ComboBox4SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string[] Familia = {"Canidae", "Felidae", "Herpestidae","Hyanidae","Mustelidae","Ursidae"};
+			if(comboBox4.Text == "Carnivora")
+			{
+				foreach(string c in Familia)
+					comboBox5.Items.Add(c);
+				comboBox5.Enabled = true;
+			}
+			else
+			{
+				comboBox5.Items.Clear();
+				comboBox5.Text = "";
+				comboBox5.Enabled = false;
+			}			
+		}
+		
+		/* Selección de la Familia */
+		void ComboBox5SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string[] Genero = {"Acinonyx", "Caracal", "Felis", "Leopardus", "Lynx", "Puma", "Panthera"};
+			if(comboBox5.Text == "Felidae")
+			{
+				foreach(string c in Genero)
+					comboBox6.Items.Add(c);
+				comboBox6.Enabled = true;
+			}
+			else
+			{
+				comboBox6.Items.Clear();
+				comboBox6.Text = "";
+				comboBox6.Enabled = false;
+			}	
+		}
+		
+		/* Selección de la Especie */
+		void ComboBox6SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string[] Especie = {"bieti", "catus", "chaus", "margarita", "nigripes", "silvestris"};
+			if(comboBox6.Text == "Felis")
+			{
+				foreach(string c in Especie)
+					comboBox7.Items.Add(c);
+				comboBox7.Enabled = true;
+			}
+			else
+			{
+				comboBox7.Items.Clear();
+				comboBox7.Text = "";
+				comboBox7.Enabled = false;
+			}
+		}
+		
+		/* Selección de la Raza */
+		void ComboBox7SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string[] Raza = {"Abisinio", "Angora", "Bengala", "Bombay", "Bosque de Noruega", "Cornish rex", "Europeo común", "Himalayo", "Ocicat", "Oriental", "Persa", "Siamés", "Siberiano"};
+			if(comboBox7.Text == "catus")
+			{
+				foreach(string c in Raza)
+					comboBox8.Items.Add(c);
+				comboBox8.Enabled = true;
+			}
+			else
+			{
+				comboBox8.Items.Clear();
+				comboBox8.Text = "";
+				comboBox8.Enabled = false;
+			}	
+		}
+		
+		void ComboBox8SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if(comboBox8.Text != "")
+			{
+				button1.Enabled = true;
+			}
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			// Alinear el texto a la mitad
+			label9.TextAlign = ContentAlignment.MiddleCenter;
+			// La propiead Font es de solo lectura, por lo que se debe crear mediante un constructor
+			label9.Font = new Font("Arial", 16.0F, FontStyle.Bold);
+			// Cambiar el color
+			label9.ForeColor = Color.Red;
+			label9.Text = "Es un gato " + comboBox8.Text;
+		}
+	}
+}
