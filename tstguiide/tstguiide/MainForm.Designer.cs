9a1b38cedbbc8c6b65bd06e4e66918214685e730
NightRoadIx <quintanarxbio@gmail.com>
2020-04-15 03:24:46 -0500

    Add files via upload

---
+++ tstguiide/tstguiide/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: NightRoadIx
+ * Date: 15/04/2020
+ * Time: 2:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace tstguiide
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.richTextBox1.Location = new System.Drawing.Point(25, 37);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(225, 190);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.numericUpDown1.Location = new System.Drawing.Point(25, 233);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+									1000,
+									0,
+									0,
+									0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(225, 20);
+			this.numericUpDown1.TabIndex = 1;
+			this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1ValueChanged);
+			// 
+			// monthCalendar1
+			// 
+			this.monthCalendar1.Location = new System.Drawing.Point(262, 37);
+			this.monthCalendar1.Name = "monthCalendar1";
+			this.monthCalendar1.TabIndex = 2;
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(262, 233);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(227, 20);
+			this.dateTimePicker1.TabIndex = 3;
+			this.dateTimePicker1.ValueChanged += new System.EventHandler(this.DateTimePicker1ValueChanged);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(25, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(225, 23);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Ingresar un texto:";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			this.label1.MouseLeave += new System.EventHandler(this.Label1MouseLeave);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(25, 266);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(225, 19);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "label2";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(262, 266);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(226, 19);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "label3";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(576, 309);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.monthCalendar1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.richTextBox1);
+			this.Name = "MainForm";
+			this.Text = "tstguiide";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.MonthCalendar monthCalendar1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+	}
+}
---
+++ tstguiide/tstguiide/MainForm.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: NightRoadIx
+ * Date: 15/04/2020
+ * Time: 2:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace tstguiide
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+			// Modificar algunos de los elementos de la caja de texto enriquecido
+			richTextBox1.BackColor =  Color.Black;
+			richTextBox1.ForeColor = Color.Lime;
+			richTextBox1.Font = new Font("Console", 12);
+			
+			// Modificar el control numérico
+			numericUpDown1.BackColor = Color.Fuchsia;
+			// colocar los valores que serán de 
+			// mínimo
+			numericUpDown1.Minimum = 0;
+			// máximo del control
+			numericUpDown1.Maximum = 1000;
+			// y el incremento
+			numericUpDown1.Increment = 1;
+			// default
+			numericUpDown1.Value = 666;
+			
+			label2.Text = "Valor de la cosa " + numericUpDown1.Value;
+			
+			// Modificar el Date Time Picker
+			dateTimePicker1.Font = new Font("Comic Sans MS", 8);
+			
+			// Modificar a idioma español con Culture de Globalization
+			CultureInfo cult = CultureInfo.CreateSpecificCulture("es-MX");
+			// Crear un objeto de tipo DateTime que presenta la fecha
+			DateTime fecha;
+			fecha = dateTimePicker1.Value;
+			label3.Text = "Hoy es " + cult.DateTimeFormat.GetDayName(fecha.DayOfWeek) + " día de guardar";
+			
+		}
+		
+		void NumericUpDown1ValueChanged(object sender, EventArgs e)
+		{
+			label2.Text = "Valor de la cosa " + numericUpDown1.Value;
+		}
+		
+		void DateTimePicker1ValueChanged(object sender, EventArgs e)
+		{
+			/* Esto podría moverse a una variable global para no volver a declarar todo */
+			// Modificar a idioma español con Culture de Globalization
+			CultureInfo cult = CultureInfo.CreateSpecificCulture("es-MX");
+			// Crear un objeto de tipo DateTime que presenta la fecha
+			DateTime fecha;
+			fecha = dateTimePicker1.Value;
+			label3.Text = "Hoy es " + cult.DateTimeFormat.GetDayName(fecha.DayOfWeek) + " día de guardar";
+		}
+		
+		void Label1Click(object sender, EventArgs e)
+		{
+			label1.Text = "No me des click!";
+			label1.Font = new Font("Comic Snas MS", 14);
+			label1.ForeColor  = Color.Red;
+		}
+		
+		void Label1MouseLeave(object sender, EventArgs e)
+		{
+			label1.Text = "Ingresar un texto:";
+			label1.Font = new Font("Microsoft Sans Serif", 8);
+			label1.ForeColor  = Color.Black;
+		}
+	}
+}
