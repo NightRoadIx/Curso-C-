eac5704d7b108482de0cab6530a1141a07253b1c
NightRoadIx <quintanarxbio@gmail.com>
2020-07-24 01:19:31 -0500

    Update beispiel06.cs

--- beispiel06.cs
+++ beispiel06.cs
@@ -1,3 +1,14 @@
+/*
+ * Arreglos
+ * Bloque de Memoria
+ * 
+ * | Espacio de memoria para un Entero 32 bits | => int 
+ * 
+ * Arreglos espacio en memoria del tamaño del tipo del variable
+ * | int32 espacio 01 | [0] => int arreglo
+ * | int32 espacio 02 | [1] ...
+ * 
+ * */
 using System;
 
 namespace pruebas
@@ -9,11 +20,14 @@ namespace pruebas
 			
 			// Genear un vector con valores ya definidos
 			int[] misNumeros = {0,1,2,3,4,5,6,7,8,9};
+			// | 1,2,3,4,5,6,7,8,9 |
 			// Generar un vector con cierta cantidad de espacios disponibles
 			// se debe usar la palabra new y el tipo de dato, de la forma:
 			// int[] nombre = new int[TAMAÑO];
 			
-			// Sin embargo es posible usar asiganción dinámica de espacios
+			// C-ANSI:
+			// int vector[10];
+			// Sin embargo, es posible usar asignación dinámica de espacios
 			Console.Write("Ingresar tamaño del vector: ");
 			int k = Convert.ToInt32(Console.ReadLine());
 			int[] vector = new int[k];
@@ -21,7 +35,7 @@ namespace pruebas
 			// Así ya se pueden ingresar valores en el vector generado
 			// la propiedad .Length del vector proporciona el número
 			// total de elementos en el arreglo
-			for(k = 0; k < vector.Length, k++)
+			for(k = 0; k < vector.Length; k++)
 			{
 				Console.Write("Elemento {0}: ", k);
 				vector[k] = Convert.ToInt32(Console.ReadLine());
@@ -32,6 +46,11 @@ namespace pruebas
 				Console.Write(" {0} ", tmp);
 			
 			// Matrices regulares
+			/*
+			 * | 0 0 0 |
+			 * | 0 0 0 |
+			 * | 0 0 0 |
+			 * */
 			// Se declaran, colocando una coma por cada dimensión
 			int[,] matriz = new int[3,3];
 			
