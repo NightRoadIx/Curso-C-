e2aa31f3a777412509d9f5189bae986c33689c4c
NightRoadIx <quintanarxbio@gmail.com>
2020-07-24 00:24:18 -0500

    Update beispiel04.cs

--- beispiel04.cs
+++ beispiel04.cs
@@ -1,26 +1,40 @@
+/*
+ * Sentencias de control
+ * para la generación de ciclos
+ * */
 using System;
 
-public class Program
+public class Program 
 {
 	public static void Main()
 	{
+		// unsigned int i;  // Entero sin signo (+) 0b11111111 => 0 a 255
+		byte ochoBits; // -127 a +127		0b01111111 
+		// 2123 = 2*10^3 + 1*10^2 + 2*10^1 + 3*10^0		
 		int i;
 		
 		// Ciclo while()
 		// El cual piensa antes de hacer
 		// se ejecuta mientras la condición sea verdadera
 		i = 0;
-		while(i < 5)
+		while(i < 5)  //true se ejecuta, false se detiene
 		{
 			Console.WriteLine(i);
 			i++;	// No olvidar modificar la variable de la condición
 		}
-		
+		/* while(true)
+		{
+			Console.WriteLine(i);
+			i++;	// No olvidar modificar la variable de la condición
+			if(i > 5) // para ciclos infinitos
+				break;
+		} */
+				
 		Console.WriteLine("\n");
 		
 		// Ciclo do - while()
 		// El cual hace (al menos una vez) y luego piensa
-		i = 0;
+		i = 0;   // i = 6;
 		do
 		{
 			Console.WriteLine(i);
@@ -53,7 +67,7 @@ public class Program
 			// Cuando i sea igual a 4, salta
 			if(i == 4)
 			{
-				i++;	// Como las líneas no se ejcutarán hay que incrementar
+				i++;	// Como las líneas no se ejecutarán hay que incrementar
 				continue;
 			}
 			// todas las instrucciones por debajo
@@ -61,5 +75,6 @@ public class Program
 			i++;
 		}
 		
+		Console.ReadKey();
 	}
 }
