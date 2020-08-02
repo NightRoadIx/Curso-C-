ffb5efa3e1443f0486dd114e00ae8a1c17f63b4b
NightRoadIx <quintanarxbio@gmail.com>
2020-08-02 01:58:13 -0500

    Add files via upload

---
+++ poblacionizacion/poblacionizacion/MainForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: NightRoadIx
+ * Date: 02/08/2020
+ * Time: 0:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace poblacionizacion
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(213, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Seleccione un país:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 35);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(213, 21);
+			this.comboBox1.TabIndex = 1;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(12, 103);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(213, 21);
+			this.comboBox2.TabIndex = 3;
+			this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2SelectedIndexChanged);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(12, 77);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(213, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Seleccione un año:";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(13, 203);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Población:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Enabled = false;
+			this.textBox1.Location = new System.Drawing.Point(119, 200);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(174, 20);
+			this.textBox1.TabIndex = 5;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Enabled = false;
+			this.textBox2.Location = new System.Drawing.Point(119, 226);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(174, 20);
+			this.textBox2.TabIndex = 7;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(13, 229);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Mínimo:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Enabled = false;
+			this.textBox3.Location = new System.Drawing.Point(119, 252);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(174, 20);
+			this.textBox3.TabIndex = 9;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(13, 255);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 23);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "Máximo:";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Enabled = false;
+			this.textBox4.Location = new System.Drawing.Point(119, 278);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(174, 20);
+			this.textBox4.TabIndex = 11;
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(13, 281);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 23);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "Promedio:";
+			// 
+			// label7
+			// 
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(14, 157);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(248, 31);
+			this.label7.TabIndex = 12;
+			this.label7.Text = "- Población -";
+			this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(231, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(195, 112);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 13;
+			this.pictureBox1.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(440, 318);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "poblacionizacion";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ poblacionizacion/poblacionizacion/MainForm.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace poblacionizacion
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		/*Atributos (variables globales)*/
+		// Crear una lista para almacenar los encabezados
+		List<string> hutt = new List<string>();
+		// Crear una lista para almacenar los años
+		List<string> anno = new List<string>();
+		// Lista para guardar todos los datos
+		List<string[]> data = new List<string[]>();
+		
+		string[] arch = {"0"};
+		
+		/* Métodos personalizados */
+		public void cambio()
+		{
+			// Variables locales
+			List<string> tmp = new List<string>();
+			
+			int maximo = int.MinValue;
+			int minimo = int.MaxValue;
+			long total = 0;
+			double promedio = 0.0;
+			
+			// Hay que verificar el texto del combobox (o su índice también) para identificar que es lo que se va a obtener
+			// Obtener el índice del elemento seleccionado
+			int i = comboBox1.SelectedIndex + 1;
+			int j = comboBox2.SelectedIndex;
+			
+			// Obtener todos los datos de la selección
+			foreach(string[] r in data)
+			{
+				// Añadir los valores a la lista temporal
+				tmp.Add(r[i]);
+				
+				// Obtener la suma de todos los valores
+				total += Convert.ToInt32(r[i]);
+				
+				// Obtener máximo de la lista
+				if( Convert.ToInt32(r[i]) > maximo )
+					maximo = Convert.ToInt32(r[i]);
+				// Obtener el mínimo de la lista
+				if( Convert.ToInt32(r[i]) < minimo )
+					minimo = Convert.ToInt32(r[i]);
+				
+			}
+			// Obtenemos el promedio
+			promedio = (double)total / (double)tmp.Count;
+			
+			/* Esta es la zona peligrosa, dado que hay cambios cada que se
+			  * añade un elemento al combobox, es necesario colocar bloques
+			  * try - catch para evitar errores, pues los índices pueden no
+			  * coincidir 
+			  * */
+			// Se usa un bloque para cada índice (podría usarse uno en general)
+			try{
+				label3.Text = "Población " + anno[j] + ":";
+				textBox1.Text = tmp[j];
+			}
+			catch(Exception e){
+			}
+						
+			// 
+			try {
+				pictureBox1.Image = Image.FromFile(arch[i-1]);
+			}
+			catch(Exception e){
+			}
+			
+			// Mostrar los valores calculados
+			textBox2.Text = minimo.ToString();
+			textBox3.Text = maximo.ToString();
+			textBox4.Text = promedio.ToString();
+			
+			/*
+			// Se propone usar este bloque
+			// El cual incluye el uso de finally para ejecutar la muestra de los 
+			// valores calculados
+			try{
+				label3.Text = "Población " + anno[j] + ":";
+				textBox1.Text = tmp[j];
+				pictureBox1.Image = Image.FromFile(arch[i-1]);
+			}
+			catch(Exception e){
+			}
+			finally {
+				// Mostrar los valores calculados
+				textBox2.Text = minimo.ToString();
+				textBox3.Text = maximo.ToString();
+				textBox4.Text = promedio.ToString();
+			}*/
+		}
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		// Cuando la ventana se crea
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			// Variables locales
+			int j = 0;		// Contador de líneas
+			
+			// Lectura del archivo CSV
+			using(var lector = new StreamReader("urban_pop.csv"))
+			{
+				// Hasta el fin del archivo
+				while(!lector.EndOfStream)
+				{
+					// Leer línea por línea ('\n')
+					var linia = lector.ReadLine();
+					
+					// Encabezado
+					if(j == 0)
+					{
+						// Recorrer elemento a elemento y añadir a una lista
+						foreach(string en in linia.Split(','))
+							hutt.Add(en);
+					}
+					else
+					{
+						// Añadir los elementos de la tabla
+						data.Add(linia.Split(','));
+						// Se añaden los años
+						anno.Add( linia.Split(',')[0] );
+					}
+					// contador de líneas leídas
+					j++;
+				}
+				
+				/* Leídos los datos, comenzar a llenar los espacios de la GUI */
+				hutt.RemoveRange(0,1);
+				foreach(string p in hutt)
+					comboBox1.Items.Add(p);
+				comboBox1.Text = comboBox1.Items[0].ToString();
+				
+				// Colocar los años
+				foreach(string a in anno)
+					comboBox2.Items.Add(a);
+				comboBox2.Text = comboBox2.Items[0].ToString();
+				
+				/* Colocar las banderas */
+				// encontrar el directorio del programa
+				string directorio = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+				// localizamos los subdirectorios
+				string[] subd = Directory.GetDirectories(directorio, "img*");
+				// Obtenemos todos los archivos en el subdirectorio
+				arch = Directory.GetFiles(subd[0]);
+				// Cargar la primera imagen
+				pictureBox1.Image = Image.FromFile(arch[0]);
+			}
+		}
+		
+		void ComboBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+			cambio();
+		}
+		
+		void ComboBox2SelectedIndexChanged(object sender, EventArgs e)
+		{
+			cambio();
+		}
+	}
+}
