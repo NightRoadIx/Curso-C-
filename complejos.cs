c819458274f3fa7f788cc0887ba9bb80a64920bf
NightRoadIx <quintanarxbio@gmail.com>
2021-10-25 20:20:39 -0500

    Update complejos.cs

--- complejos.cs
+++ complejos.cs
@@ -1,17 +1,24 @@
 /*
  * Created by SharpDevelop.
  * User: s_bio
- * Date: 27/09/2021
- * Time: 06:12 p. m.
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 
-namespace complejos
-{		
-	// Clase de numeros complejos
-	// z = r + i
+namespace objetosMaximo
+{	
+	// TODO: Clase para restringir el ingreso de datos
+	// Clase 
+	//public class 
+	
+	// TODO: Documentar bien toda la clase NumComplejos
+	
+	/// <summary>
+	/// Aquí van los números complejos de la forma:
+	/// complejo = real +/- imaginario
+	/// <remarks>Aquí que hace?</remarks> 
+	/// </summary>
 	public class NumComplejos
 	{
 		/* ATRIBUTOS */
@@ -19,14 +26,14 @@ namespace complejos
 		private double imag;
 		
 		/* MÉTODOS */		
-		// Constructor por defecto
+		// Constructor por defecto, inicia todo a 0
 		public NumComplejos()
 		{
 			this.real = 0;
 			this.imag = 0;
 		}
 		
-		// Constructor con valores
+		// Constructor con valores enviados como parámetros
 		public NumComplejos(double re, double im)
 		{
 			this.real = re;
@@ -34,33 +41,28 @@ namespace complejos
 		}
 		
 		// Getters y Setters
-		// Getters
-		public double getReal()
+		public double Real
 		{
-			return this.real;
+			get; set;
 		}
-		public double getImag()
+		public double Imag
 		{
-			return this.imag;
-		}
-		// Setters
-		public void setReal(double re)
-		{
-			this.real = re;
-		}
-		public void setImag(double im)
-		{
-			this.imag = im;
+			get; set;
 		}
 		
-		// Mostrar el número complejo
-		public void mostrarComplejos()
+		// Mostrar el número complejo, aquí se va a sobrecargar el método ToString
+		public override string ToString()
 		{
-			Console.WriteLine("{0} + {1}i",this.real, this.imag);
+			return string.Format("{0} + {1}i\n", this.real, this.imag);
 		}
 		
-		// Operaciones
-		// Suma (a, b) + (c, d) = (a+c, b+d)
+		/* Operaciones */
+		/// <summary>
+		/// Suma de números complejos: 
+		///  (a, b) + (c, d) = (a+c, b+d)
+		/// </summary>
+		/// <param name="c"> objeto NumComplejos a sumar</param>
+		/// <returns> objeto NumComplejos, con el valor de la suma</returns>
 		public NumComplejos sumarComplejo(NumComplejos c)
 		{
 			NumComplejos aux = new NumComplejos();
@@ -88,41 +90,32 @@ namespace complejos
 			return aux;
 		}
 		
-		// TODO: Generar la división
+		// Divide (a, b) / (c, d) = ((a*c + b*d) / (c^2 + d^2) , (b*c – a*d) / (c^2 + d^2))
+		public NumComplejos dividirComplejo(NumComplejos c)
+		{
+			NumComplejos aux = new NumComplejos();
+			aux.real = (this.real * c.real + this.imag * c.imag) / (c.real * c.real + c.imag * c.imag);
+			aux.imag = (this.imag * c.real - this.real * c.imag) / (c.real * c.real + c.imag * c.imag);
+						
+			return aux;
+		}
+		
+		// TODO: Incuir, potencia y raíz n-ésima de los números complejos
 	}
 	
-	public class Program
+	class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.Title = "Programacización orientada a objetivización";
+			// Instanciar un objeto de número complejo con el valor 2+5i
+			NumComplejos complejo1 = new NumComplejos(2, 5);
 			
-			// Instanciar un objeto complejo1 del tipo NumComplejos
-			NumComplejos complejo1 = new NumComplejos(1, 2); // 1 + 2i
-			NumComplejos complejo2 = new NumComplejos(3, 4); // 3 + 4i			
-			NumComplejos complejo3 = new NumComplejos(); // 0 + 0i
+			// Mostrar el número complejo usando el método sobrecargado ToString()
+			Console.Write( complejo1.ToString() );
 			
-			complejo1.mostrarComplejos();
-			complejo2.mostrarComplejos();
-			complejo3.mostrarComplejos();			
-			
-			complejo1.setReal(6.0);
-			complejo1.mostrarComplejos();
-			
-			Console.WriteLine("Parte real complejo1: {0}", complejo1.getReal());
-			
-			complejo3 = complejo1.sumarComplejo(complejo2);
-			Console.Write("Suma: "); complejo3.mostrarComplejos();
-			
-			// Instanciar el objeto
-			NumComplejos complejo4 = new NumComplejos();
-			Console.Write("Escriba un número complejo: ");
-			// TODO: Leer el complejo en una sola línea????
-			// Lectura de una cadena del tipo A+-Bi
-			Console.ReadLine();
-			
-			Console.Write("Presione una tecla para continuar si fuése usted tan amable . . . ");
+			Console.Write("Presione cualquier tecla para continuar . . . ");
 			Console.ReadKey(true);
+			
 		}
 	}
 }
