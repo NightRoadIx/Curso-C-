94ee83ce85b7f421c90e812c9aa2b03603e5b60d
NightRoadIx <quintanarxbio@gmail.com>
2021-11-30 23:08:09 -0600

    Add files via upload

---
+++ FibonacciGUI/FibonacciGUI/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: s_bio
+ * Date: 30/11/2021
+ * Time: 05:11 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace FibonacciGUI
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 25);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(268, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Cantidad de números de la serie:";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(12, 77);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(356, 71);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Calcular serie";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(268, 22);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(12, 198);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(356, 174);
+			this.textBox2.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.OrangeRed;
+			this.label2.Location = new System.Drawing.Point(12, 172);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(356, 23);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "-Serie de Fibonacci-";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(383, 396);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Calculadora de series de Fibonacci";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ FibonacciGUI/FibonacciGUI/MainForm.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: s_bio
+ * Date: 30/11/2021
+ * Time: 05:11 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FibonacciGUI
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		/* VARIABLES O PROPIEADES */
+		List <int> arregloF = new List<int>();
+		int n, nim = 0, xam = 46;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		/* Cuando se de clic al botón button1 */
+		void Button1Click(object sender, EventArgs e)
+		{
+			// Revisar que los valores ingresados sean los correctos
+			if( (!Int32.TryParse(textBox1.Text, out n))||((n <= nim)||(n > xam)) )
+			{
+				MessageBox.Show("Error mortal, el número " + textBox1.Text + " está fuera del intervalo [" + nim.ToString() + "," + xam.ToString() + "]", "Error en datos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				// Una vez que se le haya dado el OK a la ventanita, pues continua con esta parte del programa
+				// Limpiar la caja de texto
+				textBox1.Text = "";
+				// Colocar el foco a la caja de texto
+				textBox1.Focus();			
+			}
+			else
+			{
+				// Limpiar la caja de texto2 para presentar la serie
+				textBox2.Text = "";
+				// Calcular la serie con los datos y llenar en la caja de texto con los valores
+				for(int k = 0, a = 1, b = 0, c = 0; k < this.n; c = a + b, a = b, b = c, k++)
+				{
+					arregloF.Add(c);
+					textBox2.Text += c.ToString() + ",";
+				}
+			}
+		}
+		
+		/* Cuando el usuario ingresa un valor a la textBox1 */
+		void TextBox1KeyPress(object sender, KeyPressEventArgs e)
+		{
+			// Con esto se asegura que solamente se ingresen valores numéricos
+			if(!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+				e.Handled = true;
+			// Cuando se presione la tecla "Enter" pues simplemente realiz aun click
+			if( e.KeyChar == (char)Keys.Return )
+				button1.PerformClick();
+		}
+	}
+}
