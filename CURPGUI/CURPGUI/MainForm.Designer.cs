52a3619b29bbc3dbe13d4c13fa3a699e202c6fe3
NightRoadIx <quintanarxbio@gmail.com>
2022-04-13 00:47:39 -0500

    Add files via upload

---
+++ CURPGUI/CURPGUI/MainForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: s_bio
+ * Date: 12/04/2022
+ * Time: 12:08 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace CURPGUI
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.label5 = new System.Windows.Forms.Label();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.label6 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(156, 19);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Nombre:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(175, 10);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(212, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(175, 38);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(212, 22);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(13, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(156, 19);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Apellido paterno:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(175, 66);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(212, 22);
+			this.textBox3.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(13, 69);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(156, 19);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Apellido materno:";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(13, 138);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(156, 19);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Sexo:";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(175, 133);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(104, 24);
+			this.radioButton1.TabIndex = 7;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Hombre";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(285, 133);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(104, 24);
+			this.radioButton2.TabIndex = 8;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "Mujer";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(13, 102);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(156, 19);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Fecha de nacimiento:";
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(175, 97);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(213, 22);
+			this.dateTimePicker1.TabIndex = 10;
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(13, 176);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(156, 19);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Lugar de nacimiento:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(175, 176);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(212, 24);
+			this.comboBox1.TabIndex = 12;
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(13, 328);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(156, 19);
+			this.label7.TabIndex = 13;
+			this.label7.Text = "CURP:";
+			// 
+			// label8
+			// 
+			this.label8.Location = new System.Drawing.Point(13, 300);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(156, 19);
+			this.label8.TabIndex = 14;
+			this.label8.Text = "RFC:";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(175, 325);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(212, 22);
+			this.textBox4.TabIndex = 16;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(175, 297);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.ReadOnly = true;
+			this.textBox5.Size = new System.Drawing.Size(212, 22);
+			this.textBox5.TabIndex = 15;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 224);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(376, 48);
+			this.button1.TabIndex = 17;
+			this.button1.Text = "Obtener";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(403, 376);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Opacity = 0.9D;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "CURPGUI";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ CURPGUI/CURPGUI/MainForm.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: s_bio
+ * Date: 12/04/2022
+ * Time: 12:08 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+// Incluir la parte de expresiones regulares
+using System.Text.RegularExpressions;
+// Incluir la parte de lectura/escritura de archivos
+using System.IO;
+
+namespace CURPGUI
+{
+	/// <summary>
+	/// Description of MainForm
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		// Diccionario para guardar la información de los estados
+		public Dictionary<string, string> estates = new Dictionary<string, string>();
+		
+		// Funciones
+		// Obtener las consonantes de la cadena
+		public string obtenerConsonantes(string cadena)
+		{
+			string conson = Regex.Replace(cadena, "(?<!^)[aeiouáéíóú](?!$)", "");
+			return conson;
+		}
+		// Obtener las vocales de la cadena
+		public string obtenerVocales(string cadena)
+		{
+			string vocale = Regex.Replace(cadena, "([^aeiouáéíóú\\W]+)", "");
+			return vocale;
+		}
+		// Eliminar los espacios de una cadena
+		public string sinEspacios(string cadena)
+		{
+			string ohneSpace = Regex.Replace(cadena, @"\s+", "");
+			return ohneSpace;
+		}
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		// Función asociada al evento carga de formulario
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			foreach(string s in File.ReadLines("estados.txt"))
+			{
+				// Separar la cadena leída con una ','
+				var linea = s.Split(',');
+				// Cargar la información en el combobox
+				comboBox1.Items.Add(linea[0]);
+				
+				// Asignar al diccionario
+				estates.Add(linea[0], linea[1]);
+			}
+		}
+		
+		// Función asociada al evento clic del ratón
+		void Button1Click(object sender, EventArgs e)
+		{
+			/* SECCIÓN DEL NOMBRE */
+			string ini = "";
+			ini += textBox2.Text[0];
+			ini += obtenerVocales(textBox2.Text)[0];
+			ini += textBox3.Text[0];
+			ini += textBox1.Text[0];
+			
+			/* SECCIÓN DE FECHA */
+			string anno = dateTimePicker1.Value.Year.ToString().Substring(2,2);			
+			int tmp = dateTimePicker1.Value.Month;
+			string mess = tmp < 10 ? "0" + tmp.ToString() : tmp.ToString();
+			tmp = dateTimePicker1.Value.Day;
+			string diaa = tmp < 10 ? "0" + tmp.ToString() : tmp.ToString();
+			
+			/* SELECCIÓN DE SEXO */
+			string sexo = "";
+			if( radioButton1.Checked )
+				sexo = "H";
+			else if( radioButton2.Checked )
+				sexo = "M";
+			else
+				sexo = "I";
+			
+			/* SELECCIÓN DE LA CLAVE DEL ESTADO */
+			string claveEstado; 
+			estates.TryGetValue(comboBox1.SelectedItem.ToString(), out claveEstado);
+			
+			/* CLAVE ADICIONAL */
+			string adic = "";
+			adic += obtenerConsonantes(textBox2.Text)[1];
+			adic += obtenerConsonantes(textBox3.Text)[1];
+			adic += obtenerConsonantes(textBox1.Text)[1];
+			
+			// Colocar el valor del CURP
+			textBox5.Text = ini.ToUpper() + anno + mess + diaa + sexo + claveEstado + adic.ToUpper();
+		}		
+	}
+}
