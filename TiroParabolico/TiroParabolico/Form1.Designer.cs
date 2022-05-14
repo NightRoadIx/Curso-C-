08a59df1174b5ca989455354eba957f0b80b5bc7
NightRoadIx <quintanarxbio@gmail.com>
2022-05-13 21:19:10 -0500

    Add files via upload

---
+++ TiroParabolico/TiroParabolico/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace TiroParabolico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.caja1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.caja2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.caja3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.caja4 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.caja5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "V_inicial";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // caja1
+            // 
+            this.caja1.Location = new System.Drawing.Point(114, 6);
+            this.caja1.Name = "caja1";
+            this.caja1.Size = new System.Drawing.Size(185, 22);
+            this.caja1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "m/s";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "°";
+            // 
+            // caja2
+            // 
+            this.caja2.Location = new System.Drawing.Point(114, 34);
+            this.caja2.Name = "caja2";
+            this.caja2.Size = new System.Drawing.Size(185, 22);
+            this.caja2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ángulo_inicial";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(305, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "m";
+            // 
+            // caja3
+            // 
+            this.caja3.Location = new System.Drawing.Point(114, 62);
+            this.caja3.Name = "caja3";
+            this.caja3.Size = new System.Drawing.Size(185, 22);
+            this.caja3.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "altura_inicial";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "gravedad";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Mercurio 3.7",
+            "Venus 8.87",
+            "Tierra 9.81",
+            "Marte 3.71",
+            "Júpiter 24.79",
+            "Saturno 10.44",
+            "Urano 8.69",
+            "Neptuno 11.15",
+            "Plutón 0.81"});
+            this.comboBox1.Location = new System.Drawing.Point(114, 118);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(185, 24);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(305, 93);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 17);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "s";
+            // 
+            // caja4
+            // 
+            this.caja4.Location = new System.Drawing.Point(114, 90);
+            this.caja4.Name = "caja4";
+            this.caja4.Size = new System.Drawing.Size(185, 22);
+            this.caja4.TabIndex = 12;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 93);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 17);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "pasos";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(305, 122);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 17);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "m/s²";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Salvar local";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 332);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(59, 17);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Archivo:";
+            // 
+            // caja5
+            // 
+            this.caja5.Location = new System.Drawing.Point(114, 329);
+            this.caja5.Name = "caja5";
+            this.caja5.Size = new System.Drawing.Size(185, 22);
+            this.caja5.TabIndex = 17;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(328, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Salvar BD";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(568, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(233, 62);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Calcular";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(514, 12);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(341, 287);
+            this.textBox6.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 419);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.caja5);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.caja4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.caja3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.caja2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.caja1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tiro Parabólico";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox caja1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox caja2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox caja3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox caja4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox caja5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ TiroParabolico/TiroParabolico/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TiroParabolico
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /* Ejecutar cuando se carga la ventana */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* Valores iniciales */
+            caja1.Text = "0.0";
+            caja2.Text = "0";
+            caja3.Text = "0.0";
+            caja4.Text = "0.0";
+            caja5.Text = "Archivo";
+            comboBox1.SelectedIndex = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Objeto Canelo con las condiciones iniciales
+            SiHayTiro canelo = new SiHayTiro(
+                double.Parse(caja1.Text),
+                int.Parse(caja2.Text),
+                double.Parse(comboBox1.SelectedItem.ToString().Split()[1]),
+                double.Parse(caja3.Text)
+                );
+            double tmp = 0.0;
+            string cadena = "dt \t x \t y \n";
+
+            // Calcular la posición del proyectil
+            while (true)
+            {
+
+                cadena += "\n" + tmp.ToString() + "\t" + canelo.Px.ToString() +
+                    "\t" + canelo.Py.ToString() + "\n";
+
+                // En otro caso incrementar tmp
+                tmp += double.Parse(caja4.Text);
+
+                // Calcula la posición
+                canelo.calcularPos(tmp);
+
+                // Realizar hasta que la altura se igual o menor a 0
+                if (canelo.Py <= 0.0) break;
+
+            }
+            textBox6.Text = cadena;
+            //File.AppendAllText(caja5.Text + ".txt", cadena);
+        }
+    }
+
+    public class SiHayTiro
+    {
+        /* Atributos */
+        public double Px
+        { get; set; }
+        public double Py
+        { get; set; }
+        public double V0
+        { get; }
+        public int Theta
+        { get; }
+        public double G
+        { get; }
+
+        /* Métodos */
+        // El constructor genera los valores iniciales para el cálculo
+        public SiHayTiro(double vIni, int aIni, double grav, double h)
+        {
+            V0 = vIni;
+            Theta = aIni; 
+            G = grav;
+            Px = 0.0;
+            Py = h;  // Altura inicial
+        }
+
+        // Función para calcular la posición
+        public void calcularPos(double t)
+        {
+            Px = V0 * Math.Cos(Theta * Math.PI / 180.0) * t;
+            Py = V0 * Math.Sin(Theta * Math.PI / 180.0) * t - 0.5 * G * Math.Pow(t,2);
+        }
+
+    }
+}
