44e8bb0792e22f6d86a51e0412e9733844192194
NightRoadIx <quintanarxbio@gmail.com>
2024-02-23 15:13:50 -0600

    Update beispiel01.cs

--- beispiel01.cs
+++ beispiel01.cs
@@ -26,12 +26,26 @@ public class Program
 		Console.WriteLine(12 ^ 10);		//Operador xor
 		Console.WriteLine(12 >> 2);		//Operador desplazamiento de bits a la derecha
 		Console.WriteLine(12 << 2);		//Operador desplazamiento de bits a la izquierda
+		Console.WriteLine(~12);			//Operador NOT para enteros
+		// Mostrar la representación en binario de un número entero
+		// OJO, este método entrega una cadena de texto
+		// no es un número operable como tal
+		Console.WriteLine(Convert.ToString(12, 2));
 		
 		// Funciones matemáticas
 		Console.WriteLine(Math.Max(5,10));		// Máximo
 		Console.WriteLine(Math.Min(5,10));		// Mínimo
 		Console.WriteLine(Math.Sqrt(81));		// Raíz Cuadrada
 		Console.WriteLine(Math.Abs(-54));		// Valor absoluto
+		Console.WriteLine(Math.Exp(2));			// Exponente (e elevado a)
+		// Funciones trigonométricas, operan en radianes
+		Console.WriteLine(Math.Cos(3.1416));		// Coseno
+		Console.WriteLine(Math.ACos(1));		// Arco coseno
+		Console.WriteLine(Math.PI);		// Constante PI
+		// Redondear PI a 4 dígitos de precisión
+		Console.WriteLine(Math.Round(Math.PI, 4));
+		// Pueden ser consultadas en:
+		// https://learn.microsoft.com/en-us/dotnet/api/system.math?view=net-8.0
 		
 		Console.ReadKey(true);
 	}
