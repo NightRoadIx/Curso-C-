8099638693b67f991607000e874ddaab64035d46
NightRoadIx <quintanarxbio@gmail.com>
2024-09-16 15:16:36 -0600

    Update Fibonaccito.cs
    Se añade la clase para crear el objeto Fibonaccito y para mostrar

--- Fibonaccito.cs
+++ Fibonaccito.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 /// <summary>
 /// Obtiene un número entero ingresado por el usuario que se encuentra dentro del intervalo [a, b].
@@ -8,20 +9,17 @@ using System;
 /// <returns>El número entero ingresado por el usuario que se encuentra dentro del intervalo especificado.</returns>
 public class Restricto
 {
-	// * * * * * * * ATRIBUTOS * * * * * * *
-	
-	// * * * * * * * MÉTODOS * * * * * * *
-	// Método especial Constructor
-	// Mismo nombre de la clase
+    // * * * * * * * MÉTODOS * * * * * * *
+    
     /// <summary>
     /// Constructor de la clase Restricto.
     /// Muestra un mensaje al crear un objeto de la clase.
-    /// </summary>	
-	public Restricto()
-	{
-		Console.WriteLine("El objeto Restricto ha sido creado");
-	}	
-		
+    /// </summary>    
+    public Restricto()
+    {
+        Console.WriteLine("El objeto Restricto ha sido creado");
+    }    
+        
     /// <summary>
     /// Obtiene un número entero ingresado por el usuario que se encuentra dentro del intervalo [a, b].
     /// Si el número ingresado no es un entero o no está dentro del rango, solicita al usuario que intente nuevamente.
@@ -45,75 +43,76 @@ public class Restricto
                 if (numero >= a && numero <= b)
                     return numero; // Retorna el número si está dentro del rango
                 Console.WriteLine($"El número debe estar entre {a} y {b}. Intenta de nuevo.");
-			}
+            }
             Console.WriteLine("El valor ingresado no es un número entero. Intenta de nuevo.");
         }
     }
 }
 
+/// <summary>
+/// Clase encargada de calcular y manejar la serie de Fibonacci.
+/// </summary>
 public class Fibonaccito
 {
-	public static void Main()
-	{
-		// Instanciar un objeto Restricto
-		// para colocar las restricciones
-		// de los datos de entrada
-		// Clase objeto = new ConstructorClase();
-		Restricto restringir = new Restricto();
-		int n = restringir.obtenerEntero(1,20);
-		
-		// Originalmente, en lenguaje C, no era posible
-		// declarar arreglos con un tamaño a partir de una
-		// variable, sin embargo, en C# es posible
-		// A esto se le conoce como arreglo dinámico
-		int[] serie = new int[n];
+    // Atributo para almacenar la serie de Fibonacci
+    private List<int> serieFibonacci;
 
-		// Otra forma de realizar esto es mediante
-		// un objeto List, el cual funciona como
-		// una lista de datos
-		// La declaración requiere del uso de una
-		// "wrapper class", que le indica a la lista
-		// que objetos va a almacenar
-		// La instanciación del objeto es:
-		// List<wrapperclass> objeto = new List<wrapperclass>()
-		List<int> serie2 = new List<int>();
+    /// <summary>
+    /// Constructor de la clase Fibonaccito.
+    /// </summary>
+    public Fibonaccito()
+    {
+        // Inicializar la lista que contendrá la serie
+        serieFibonacci = new List<int>();
+    }
 
-		/* Serie por arreglos dinámicos */
-		Console.WriteLine("\nResolución por arreglos");
-		// Revisar que n sea mayor a 1 y 2 para
-		// iniciar los valores
-		if(n > 2)
-		{
-			// Se inician los dos primeros valores
-			serie[0] = 0; serie[1] = 1;
-			// Todo puede calcularse en el for
-			// observar que si no se van a colocar 
-			// operaciones, se coloca ; al final del for
-			for(int i = 2; i < n; serie[i] = serie[i-1] + serie[i-2], i++);
-		}
+    /// <summary>
+    /// Calcula la serie de Fibonacci hasta el enésimo número y almacena los resultados en una lista.
+    /// </summary>
+    /// <param name="n">El número de términos de la serie que se desean calcular.</param>
+    public void CalcularSerie(int n)
+    {
+        // Limpiar la lista antes de calcular la serie
+        serieFibonacci.Clear();
 
-		// Mostrar cuando se calcula con arreglos dinámicos
-		// foreach es una instrucción que permite recorrer
-		// una colección de datos de manera completa
-		// foreach(tipo_dato variable in coleccion)
-		foreach(int tmp in serie)
-		{	Console.Write(tmp + ", "); }
+        for (int j = 0; j < n; j++)
+        {
+            if (j == 0) serieFibonacci.Add(0);   // El primer número de Fibonacci es 0
+            else if (j == 1) serieFibonacci.Add(1);  // El segundo número de Fibonacci es 1
+            else serieFibonacci.Add(serieFibonacci[j - 1] + serieFibonacci[j - 2]); // Suma de los dos anteriores
+        }
+    }
 
-		/* Resolución por objetos */
-		Console.WriteLine("\nResolución por objetos");
-		// La ventaja del uso de objetos es que
-		// estos, cuentan con ciertos métodos para
-		// su manejo
-		// Por ejemplo, el médodo Add() para añadir
-		// elementos a la lista
-		for(int j = 0; j < n; j++)
-		{
-			if(j == 0) serie2.Add(0);
-			else if(j== 1) serie2.Add(1);
-			else serie2.Add(serie2[j-1] + serie2[j-2]);
-		}
-		// Mostrar el resultado
-		foreach(int tmp in serie2)
-		{	Console.Write(tmp + ", "); }
-	}
+    /// <summary>
+    /// Muestra la serie de Fibonacci almacenada.
+    /// </summary>
+    public void MostrarSerie()
+    {
+        // Mostrar los elementos de la lista
+        foreach (int numero in serieFibonacci)
+        {
+            Console.Write(numero + ", ");
+        }
+        Console.WriteLine();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Instanciar un objeto Restricto para pedir la longitud de la serie
+        Restricto restringir = new Restricto();
+        int n = restringir.obtenerEntero(1, 20);
+
+        // Instanciar un objeto Fibonaccito
+        Fibonaccito fibonaccito = new Fibonaccito();
+
+        // Calcular la serie de Fibonacci hasta el enésimo número
+        fibonaccito.CalcularSerie(n);
+
+        // Mostrar la serie de Fibonacci
+        Console.WriteLine("La serie de Fibonacci es:");
+        fibonaccito.MostrarSerie();
+    }
 }
